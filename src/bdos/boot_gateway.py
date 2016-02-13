2cafa1c8f53c26dee2d72a4ef088c081920d6b0d
Steffen Karlsson <steffen.karlsson@gmail.com>
2016-02-13 14:08:06 +0100

    adding sigterm handler to remove daemon from nameserver on kill

--- src/bdos/boot_gateway.py
+++ src/bdos/boot_gateway.py
@@ -7,6 +7,13 @@ from logging import error, info
 from sys import argv, path
 from __init__ import NS
 from utils import import_class
+from signal import signal, SIGTERM, SIGINT
+
+REGISTRY_NAME = None
+
+def _handle_kill_signal(signal, frame):
+    NS.remove(REGISTRY_NAME)
+    exit(1)
 
 if __name__ == "__main__":
     config = validate_configuration()
@@ -22,10 +29,13 @@ if __name__ == "__main__":
     cls = str("%s/%s" % (config.gateway.file.replace(".py", ""), config.gateway.name)).replace("/", ".")
     instance = import_class(cls)
 
-    registry_name = "%s-%d" % (config.gateway.ns_registry_name, index)
+    REGISTRY_NAME = "%s-%d" % (config.gateway.ns_registry_name, index)
     daemon = Daemon(port=config.gateway.get_port(index))
     uri = daemon.register(instance)
-    NS.register(registry_name, uri)
+    NS.register(REGISTRY_NAME, uri)
 
-    info("Gateway ready at %s with registry: %s" % (str(uri), registry_name))
+    signal(SIGTERM, _handle_kill_signal)
+    signal(SIGINT, _handle_kill_signal)
+
+    info("Gateway ready at %s with registry: %s" % (str(uri), REGISTRY_NAME))
     daemon.requestLoop()
