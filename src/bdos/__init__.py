db428ea6e97ed6a23ba74a4bc1f85f66b3ae89f1
Steffen Karlsson <steffen.karlsson@gmail.com>
2016-02-14 17:12:39 +0100

    adding license and version

--- src/bdos/__init__.py
+++ src/bdos/__init__.py
@@ -1,6 +1,11 @@
 # Created by Steffen Karlsson on 02-12-2016
 # Copyright (c) 2016 The Niels Bohr Institute at University of Copenhagen. All rights reserved.
 
+__author__ = 'Steffen Karlsson'
+__version__ = '0.1-dev'
+__licence__ = 'GNU LESSER GENERAL PUBLIC LICENSE Version 3'
+__doc__ = ""
+
 from sys import path
 from os import getcwd
 path.append(getcwd())
