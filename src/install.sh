c4e88e50e78b1775d8b01dba9ae06dc14d8aebf8
Steffen Karlsson <steffen.karlsson@gmail.com>
2016-02-15 21:20:47 +0100

    improved install script

--- src/install.sh
+++ src/install.sh
@@ -3,6 +3,24 @@
 # Created by Steffen Karlsson on 02-12-2016
 # Copyright (c) 2016 The Niels Bohr Institute at University of Copenhagen. All rights reserved.
 
+validate_bdos () {
+    pythonlocallib='/usr/lib/python2.7/dist-packages/bdos'
+    pythonlib='/usr/local/lib/python2.7/dist-packages/bdos'
+
+    if [ -d "$pythonlocallib" ] ;
+    then
+        bdosdir=${pythonlocallib}
+    elif [ -d "$pythonlib" ] ;
+    then
+        bdosdir=${pythonlib}
+    else
+        echo "Aborting, please try to run the install.sh script again to succesfully install bdos"
+        exit 1
+    fi
+}
+
 python setup.py sdist
 pip install dist/bdos-0.1.dev0.tar.gz
-make html -C docs/
+
+validate_bdos
+make html -C ${bdosdir}/web/docs/
