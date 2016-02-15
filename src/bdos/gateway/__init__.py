edce7acda532b02c9b478b08904b032a4112f62f
Steffen Karlsson <steffen.karlsson@gmail.com>
2016-02-15 21:23:38 +0100

    updated gateway with temp folder to write dataset class to

--- src/bdos/gateway/__init__.py
+++ src/bdos/gateway/__init__.py
@@ -1,7 +1,7 @@
 # Created by Steffen Karlsson on 02-11-2016
 # Copyright (c) 2016 The Niels Bohr Institute at University of Copenhagen. All rights reserved.
 
-from handler import GatewayApi, GatewayHandler
+from handler import GatewayApi, GatewayHandler, CLASS_ERROR
 
 api = GatewayApi
 handler = GatewayHandler
--- src/bdos/gateway/handler.py
+++ src/bdos/gateway/handler.py
@@ -1,31 +1,46 @@
 # Created by Steffen Karlsson on 02-11-2016
 # Copyright (c) 2016 The Niels Bohr Institute at University of Copenhagen. All rights reserved.
 
-from Pyro4 import Proxy
+from inspect import getsourcefile
 
-from bdos import NS
+from Pyro4 import Proxy, locateNS
 
-GATEWAY_URI_NAMESPACE = "bdos.gateway"
+from bdos.utils import find_identifier, import_class_from_source, import_class
+import bdos.tmp
+
+CLASS_ERROR = "ExpectedDatasetClassNotExists"
 
 
 class GatewayHandler(object):
-    def create(self, name):
-        print "New dataset with name: ", name
+    def create(self, name, dataset_source, dataset_type_name):
+        tmp_location = bdos.tmp.__file__.rsplit("/", 1)[0]
+        new_file = '%s/%s.py' % (tmp_location, name)
+        with open(new_file, 'w+') as f:
+            f.write(dataset_source)
+
+        cls_inst = import_class_from_source(new_file, dataset_type_name)
+        if cls_inst is None:
+            return CLASS_ERROR
+
+        identifier = find_identifier(name)
+        dataset_cls = cls_inst.instance(identifier)
+        print "New dataset with name: ", name, " created."
+        return identifier
 
     def append(self, name, url):
         print "Adding %s to %s" % (url, name)
 
 
 class GatewayApi(object):
-
     def __init__(self, gateway_uri):
         self.uri = gateway_uri
 
     def __get_gateway_handler(self):
-        return Proxy(NS.lookup(self.uri))
+        return Proxy(locateNS().lookup(self.uri))
 
-    def create(self, name):
-        return self.__get_gateway_handler().create(name)
+    def create(self, name, dataset_type_name):
+        with open(getsourcefile(import_class(dataset_type_name)), "r") as f:
+            return self.__get_gateway_handler().create(name, f.read(), dataset_type_name)
 
     def append(self, name, url):
         self.__get_gateway_handler().append(name, str(url))
