e641127c7ac095f233edf8d5f782edbe71614dbf
Steffen Karlsson <steffen.karlsson@gmail.com>
2016-03-12 14:14:28 +0100

    add support for cache types with args like defaultdict

--- src/bdae/cache.py
+++ src/bdae/cache.py
@@ -2,20 +2,21 @@
 # Copyright (c) 2016 The Niels Bohr Institute at University of Copenhagen. All rights reserved.
 
 class CacheSystem:
-    def __init__(self, cache_type):
-        self.__data = cache_type()
+    def __init__(self, cache_type, args=None):
+        if args:
+            self.__data = cache_type(args)
+        else:
+            self.__data = cache_type()
 
     def put(self, key, value):
         self.__data[key] = value
 
     def get(self, key):
-        if key not in self.__data:
-            return None
-
         return self.__data[key]
 
     def delete(self, key):
-        del self.__data[key]
+        if self.contains(key):
+            del self.__data[key]
 
     def contains(self, key):
         return key in self.__data
