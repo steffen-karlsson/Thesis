1f31687bea86d05ffb3b363c1f5c8d08dd6f5d97
Steffen Karlsson <steffen.karlsson@gmail.com>
2016-03-31 13:19:30 +0200

    adding templates folder to bdae for generic text data etc.

---
+++ code/bdae-project/bdae/templates/import_utils.py
@@ -0,0 +1,40 @@
+# Created by Steffen Karlsson on 03-31-2016
+# Copyright (c) 2016 The Niels Bohr Institute at University of Copenhagen. All rights reserved.
+
+from inspect import getmembers, isfunction, isbuiltin
+from logging import warn
+
+
+def map_function_binder(new_name, py_fun):
+    def _wrapped_py_fun(args):
+        block_gen = args[0]
+        for block in block_gen:
+            block = " ".join(block)
+            yield sum(py_fun(block, args[i]) for i in xrange(1, len(args)))
+
+    _wrapped_py_fun.__name__ = new_name
+    return _wrapped_py_fun
+
+
+def reduce_function_binder(new_name, py_fun):
+    def _wrapped_py_fun(args):
+        return py_fun(args)
+
+    _wrapped_py_fun.__name__ = new_name
+    return _wrapped_py_fun
+
+
+def module_binder(module, func_binder, funs, new_fun_names=None):
+    wrapped_functions = []
+    desired_functions = [o for o in getmembers(module) if (isfunction(o[1]) or isbuiltin(o[1])) and o[0] in funs]
+
+    use_original_names = not new_fun_names or len(desired_functions) != len(new_fun_names)
+    if use_original_names:
+        warn("New names are not used, since the count isn't equal to number of desired functions")
+
+    for idx, (name, fun) in enumerate(desired_functions):
+        if not use_original_names:
+            name = new_fun_names[idx]
+        wrapped_functions.append(func_binder(name, fun))
+
+    return wrapped_functions
---
+++ code/bdae-project/bdae/templates/text_dataset.py
@@ -0,0 +1,38 @@
+# Created by Steffen Karlsson on 03-31-2016
+# Copyright (c) 2016 The Niels Bohr Institute at University of Copenhagen. All rights reserved.
+
+from abc import ABCMeta
+import string
+import __builtin__
+
+from nltk import word_tokenize, sent_tokenize
+
+from bdae.templates.import_utils import map_function_binder, module_binder, reduce_function_binder
+
+from bdae.dataset import AbsDatasetContext
+
+
+class _TextData(AbsDatasetContext):
+    __metaclass__ = ABCMeta
+
+    def get_map_functions(self):
+        return module_binder(string, map_function_binder, ['count'], new_fun_names=['count_occurrences'])
+
+    def get_reduce_functions(self):
+        return module_binder(__builtin__, reduce_function_binder, ['sum', 'min', 'max'])
+
+
+class TextDataByWord(_TextData):
+    __metaclass__ = ABCMeta
+
+    def next_entry(self, data):
+        for word in word_tokenize(data):
+            yield word
+
+
+class TextDataBySentence(_TextData):
+    __metaclass__ = ABCMeta
+
+    def next_entry(self, data):
+        for sentence in sent_tokenize(data):
+            yield sentence
