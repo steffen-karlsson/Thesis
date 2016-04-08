eb69cc029e9ed099fc7a0d57e7ce5a730a35cf49
Steffen Karlsson <steffen.karlsson@gmail.com>
2016-04-08 11:05:56 +0200

    initial numpy array template

---
+++ code/bdae-project/bdae/templates/number_dataset.py
@@ -0,0 +1,18 @@
+# Created by Steffen Karlsson on 04-08-2016
+# Copyright (c) 2016 The Niels Bohr Institute at University of Copenhagen. All rights reserved.
+
+from abc import ABCMeta
+import numpy
+
+from bdae.templates.import_utils import map_function_binder, module_binder, reduce_function_binder
+from bdae.dataset import AbsMapReduceDataset
+
+
+class NumpyArrayDataset(AbsMapReduceDataset):
+    __metaclass__ = ABCMeta
+
+    def get_map_functions(self):
+        return module_binder(numpy, map_function_binder, ['floor, ceil, round'], new_fun_names=['count_occurrences'])
+
+    def get_reduce_functions(self):
+        return module_binder(numpy, reduce_function_binder, ['sum', 'min', 'max'])
