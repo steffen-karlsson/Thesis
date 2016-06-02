28a931c3a5ee809c58b1b8fb4820e447eb1769c3
Steffen Karlsson <steffen.karlsson@gmail.com>
2016-06-02 16:00:10 +0200

    Input validation and okay button

--- code/bdae-app/app/src/main/java/dk/steffenkarlsson/sofa/bdae/ConfigurationFlowActivity.java
+++ code/bdae-app/app/src/main/java/dk/steffenkarlsson/sofa/bdae/ConfigurationFlowActivity.java
@@ -5,8 +5,10 @@ import android.animation.ArgbEvaluator;
 import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.PorterDuff;
-import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Patterns;
 import android.view.View;
 import android.view.animation.DecelerateInterpolator;
 import android.view.inputmethod.InputMethodManager;
@@ -16,8 +18,12 @@ import android.widget.LinearLayout;
 import com.rengwuxian.materialedittext.MaterialEditText;
 import com.squareup.otto.Subscribe;
 
+import java.util.ArrayList;
+
 import butterknife.Bind;
+import butterknife.OnClick;
 import dk.steffenkarlsson.sofa.bdae.event.TransitionAnimationEndedEvent;
+import dk.steffenkarlsson.sofa.bdae.extra.ConfigurationHandler;
 
 /**
  * Created by steffenkarlsson on 6/1/16.
@@ -35,6 +41,9 @@ public class ConfigurationFlowActivity extends BaseActivity {
     @Bind(R.id.logo)
     protected ImageView mLogo;
 
+    @Bind(R.id.okay)
+    protected ImageView mOkay;
+
     @Bind(R.id.inputInstanceName)
     protected MaterialEditText mInputInstanceName;
 
@@ -44,9 +53,84 @@ public class ConfigurationFlowActivity extends BaseActivity {
     @Bind(R.id.inputGateway)
     protected MaterialEditText mInputGateway;
 
+    private ArrayList<Boolean> mValidator = new ArrayList<Boolean>(3) {{
+        add(false);
+        add(false);
+        add(false);
+    }};
+
+    private class EmptyTextWatcher implements TextWatcher {
+
+        private final MaterialEditText mEditText;
+        private final int mIndex;
+
+        public EmptyTextWatcher(MaterialEditText editText, int index) {
+            this.mEditText = editText;
+            this.mIndex = index;
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            boolean isEmpty = TextUtils.isEmpty(s.toString());
+            mValidator.add(mIndex, isEmpty);
+            if (isEmpty)
+                mEditText.setError("Required Field");
+
+            validate();
+        }
+    }
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onResume() {
+        super.onResume();
+
+        mOkay.setColorFilter(getResources().getColor(R.color.colorPrimaryDark), PorterDuff.Mode.SRC_ATOP);
+
+        mInputInstanceName.addTextChangedListener(new EmptyTextWatcher(mInputInstanceName, 0));
+        mInputGateway.addTextChangedListener(new EmptyTextWatcher(mInputInstanceName, 2));
+        mInputApiHostname.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                boolean validated = false;
+                if (TextUtils.isEmpty(s.toString()))
+                    mInputApiHostname.setError("Required Field");
+                else {
+                    if (!isValidIPPort(s.toString()))
+                        mInputApiHostname.setError("Has to be a valid ip:port");
+                    else
+                        validated = true;
+                }
+                mValidator.add(1, validated);
+                validate();
+            }
+        });
+    }
+
+    @OnClick(R.id.okay)
+    public void onOkayClicked() {
+        if (mOkay.isEnabled()) {
+            ConfigurationHandler.getInstance().setup(
+                    mInputApiHostname.getText().toString(),
+                    mInputInstanceName.getText().toString(),
+                    mInputGateway.getText().toString());
+            this.finish();
+        }
     }
 
     @Subscribe
@@ -66,7 +150,8 @@ public class ConfigurationFlowActivity extends BaseActivity {
                         anim.setEvaluator(new ArgbEvaluator());
                         anim.addListener(new Animator.AnimatorListener() {
                             @Override
-                            public void onAnimationStart(Animator animation) { }
+                            public void onAnimationStart(Animator animation) {
+                            }
 
                             @Override
                             public void onAnimationEnd(Animator animation) {
@@ -74,10 +159,12 @@ public class ConfigurationFlowActivity extends BaseActivity {
                             }
 
                             @Override
-                            public void onAnimationCancel(Animator animation) { }
+                            public void onAnimationCancel(Animator animation) {
+                            }
 
                             @Override
-                            public void onAnimationRepeat(Animator animation) { }
+                            public void onAnimationRepeat(Animator animation) {
+                            }
                         });
                         anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
                             @Override
@@ -114,6 +201,25 @@ public class ConfigurationFlowActivity extends BaseActivity {
         view.requestFocus();
         InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
         imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT);
+    }
+
+    private boolean isValidIPPort(String input) {
+        if (!input.contains(":"))
+            return false;
+
+        String[] ipport = input.split(":");
+        if (ipport.length == 0 || ipport.length == 1)
+            return false;
+
+        return Patterns.IP_ADDRESS.matcher(ipport[0]).matches() && TextUtils.isDigitsOnly(ipport[1]);
+    }
+
+    private void validate() {
+        boolean validated = mValidator.contains(false);
+        mOkay.setColorFilter(validated
+                ? getResources().getColor(R.color.white)
+                : getResources().getColor(R.color.colorPrimaryDark), PorterDuff.Mode.SRC_ATOP);
+        mOkay.setEnabled(validated);
     }
 
     @Override
