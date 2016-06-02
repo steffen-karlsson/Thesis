f602a5bb232f028b56bc751a129eda29668b2f8e
Steffen Karlsson <steffen.karlsson@gmail.com>
2016-06-02 19:43:47 +0200

    close icon

--- code/bdae-app/app/src/main/java/dk/steffenkarlsson/sofa/bdae/SubmitJobActivity.java
+++ code/bdae-app/app/src/main/java/dk/steffenkarlsson/sofa/bdae/SubmitJobActivity.java
@@ -4,6 +4,15 @@ package dk.steffenkarlsson.sofa.bdae;
  * Created by steffenkarlsson on 6/2/16.
  */
 public class SubmitJobActivity extends BaseConfigurationActivity {
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if(getSupportActionBar() != null)
+            getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_clear_white);
+    }
+
     @Override
     protected boolean requiresConfiguration() {
         return true;
@@ -12,6 +21,11 @@ public class SubmitJobActivity extends BaseConfigurationActivity {
     @Override
     protected int getLayoutResource() {
         return R.layout.activity_submit_job;
+    }
+
+    @Override
+    protected boolean showBackButton() {
+        return true;
     }
 
     @Override
