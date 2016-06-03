de515f753eef2780fb8e714e96c5cbe09e06d08f
Steffen Karlsson <steffen.karlsson@gmail.com>
2016-06-03 15:02:16 +0200

    Dashboard view with job recycler view

---
+++ code/bdae-app/app/src/main/java/dk/steffenkarlsson/sofa/bdae/recycler/SubmittedJobRecyclerView.java
@@ -0,0 +1,88 @@
+package dk.steffenkarlsson.sofa.bdae.recycler;
+
+import android.content.Context;
+import android.graphics.PorterDuff;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import dk.steffenkarlsson.sofa.bdae.R;
+import dk.steffenkarlsson.sofa.bdae.entity.Dataset;
+import dk.steffenkarlsson.sofa.bdae.entity.Job;
+
+/**
+ * Created by steffenkarlsson on 6/3/16.
+ */
+public class SubmittedJobRecyclerView extends BaseRecyclerView<Job> {
+
+    @BindView(R.id.name)
+    protected TextView mName;
+
+    @BindView(R.id.identifier)
+    protected TextView mIdentifier;
+
+    @BindView(R.id.data_type)
+    protected ImageView mDataTypeIcon;
+
+    @BindView(R.id.parameters)
+    protected TextView mParameters;
+
+    @BindView(R.id.dataset_name)
+    protected TextView mDatasetName;
+
+    @BindView(R.id.status)
+    protected TextView mStatus;
+
+    public SubmittedJobRecyclerView(Context context) {
+        super(context);
+    }
+
+    public SubmittedJobRecyclerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void setTabletContent(Job data, boolean isGridLayoutManager) { }
+
+    @Override
+    protected void setPhoneContent(Job data) {
+        mName.setText(data.getName());
+        mIdentifier.setText(data.getIdentifier());
+        mDatasetName.setText(data.getDatasetName());
+        if (data.getParameters().isEmpty())
+            mParameters.setText(R.string.job_no_parameters);
+        else
+            mParameters.setText(TextUtils.join(",", data.getParameters()));
+
+        switch (data.getStatus()) {
+            case Done:
+                mDataTypeIcon.setVisibility(VISIBLE);
+                switch (data.getDataType()) {
+                    case "image":
+                        mDataTypeIcon.setImageResource(R.drawable.ic_image_white);
+                        break;
+                    default:
+                        mDataTypeIcon.setImageResource(R.drawable.ic_text_white);
+                        break;
+                }
+                mDataTypeIcon.setColorFilter(getResources().getColor(R.color.colorImageTint),
+                        PorterDuff.Mode.SRC_ATOP);
+
+                mStatus.setText(getResources().getString(R.string.job_done));
+                mStatus.setTextColor(getResources().getColor(R.color.colorDone));
+                break;
+            case Processing:
+                mDataTypeIcon.setVisibility(GONE);
+                mStatus.setText(getResources().getString(R.string.job_processing));
+                mStatus.setTextColor(getResources().getColor(R.color.colorProcessing));
+                break;
+        }
+    }
+
+    @Override
+    protected int getLayoutResource(boolean isTablet, boolean isGridLayoutManager) {
+        return R.layout.item_job;
+    }
+}
--- code/bdae-app/app/src/main/java/dk/steffenkarlsson/sofa/bdae/view/BottomBarDashboardView.java
+++ code/bdae-app/app/src/main/java/dk/steffenkarlsson/sofa/bdae/view/BottomBarDashboardView.java
@@ -3,25 +3,29 @@ package dk.steffenkarlsson.sofa.bdae.view;
 import android.content.Context;
 import android.support.design.widget.FloatingActionButton;
 import android.util.AttributeSet;
-import android.view.View;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.OnClick;
 import dk.steffenkarlsson.sofa.bdae.IActivityHandler;
 import dk.steffenkarlsson.sofa.bdae.R;
 import dk.steffenkarlsson.sofa.bdae.SubmitJobActivity;
+import dk.steffenkarlsson.sofa.bdae.entity.Job;
+import dk.steffenkarlsson.sofa.bdae.extra.CustomRequestListener;
 import dk.steffenkarlsson.sofa.bdae.extra.TransitionAnimation;
-import dk.steffenkarlsson.sofa.bdae.request.GetDatasetsRequest;
-import dk.steffenkarlsson.sofa.networking.BaseRequest;
+import dk.steffenkarlsson.sofa.bdae.recycler.GenericRecyclerViewModel;
+import dk.steffenkarlsson.sofa.bdae.recycler.SubmittedJobRecyclerView;
+import dk.steffenkarlsson.sofa.bdae.request.GetSubmittedJobsRequest;
 import dk.steffenkarlsson.sofa.networking.Result;
 
 /**
  * Created by steffenkarlsson on 5/31/16.
  */
-public class BottomBarDashboardView extends BasePagerControllerView {
+public class BottomBarDashboardView extends BottomPagerControllerRecyclerView {
 
     public enum RequestType {
-        DATASETS
+        JOBS
     }
 
     @BindView(R.id.fab)
@@ -44,25 +48,33 @@ public class BottomBarDashboardView extends BasePagerControllerView {
     public void setContent(IActivityHandler handler) {
         super.setContent(handler);
 
-        new GetDatasetsRequest()
+        CustomRequestListener listener = new CustomRequestListener(mActivityHandler) {
+            @Override
+            protected void onData(int id, Result result) {
+                List<Job> jobs = (List<Job>) result.response;
+
+                for (Job job : jobs) {
+                    mAdapter.add(new GenericRecyclerViewModel(job, SubmittedJobRecyclerView.class));
+                }
+
+                mAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onError(int id, int statusCode) {
+                //TODO: Handle error
+            }
+        };
+
+        new GetSubmittedJobsRequest()
                 .withContext(handler.getActivity())
-                .setOnRequestListener(mListener)
-                .run(RequestType.DATASETS);
+                .setOnRequestListener(listener)
+                .run(RequestType.JOBS);
     }
 
     @Override
     protected int getLayoutResource() {
         return R.layout.content_dashboard_view;
-    }
-
-    @Override
-    public boolean shouldCache() {
-        return true;
-    }
-
-    @Override
-    public View getRoot() {
-        return this;
     }
 
     @OnClick(R.id.fab)
@@ -71,32 +83,4 @@ public class BottomBarDashboardView extends BasePagerControllerView {
                 mActivityHandler.getContext(), SubmitJobActivity.class, false),
                 TransitionAnimation.IN_FROM_BOTTOM);
     }
-
-    private BaseRequest.OnRequestListener mListener = new BaseRequest.OnRequestListener() {
-
-        @Override
-        public void onError(int id, int statusCode) {
-            //TODO: Handle error
-        }
-
-        @Override
-        public void onError(int id, int statusCode, Object error) {
-        }
-
-        @Override
-        public void onSuccess(int id, Result result) {
-            switch (result.statusCode) {
-                case 204:
-                    mActivityHandler.setNoDataVisible(true);
-                    break;
-                default:
-                    break;
-            }
-            mActivityHandler.setLoadingSpinnerVisible(false);
-        }
-
-        @Override
-        public void onProcessing() {
-        }
-    };
 }
