7f20089e3255e0721290643d9b4a37d693c7fbbe
Steffen Karlsson <steffen.karlsson@gmail.com>
2016-06-03 22:58:05 +0200

    typeface span

---
+++ code/bdae-app/app/src/main/java/dk/steffenkarlsson/sofa/bdae/extra/CustomTypefaceSpan.java
@@ -0,0 +1,43 @@
+package dk.steffenkarlsson.sofa.bdae.extra;
+
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.text.TextPaint;
+import android.text.style.MetricAffectingSpan;
+
+/**
+ * Created by steffenkarlsson on 6/2/16.
+ */
+public class CustomTypefaceSpan extends MetricAffectingSpan {
+    private final Typeface mTypeface;
+
+    public CustomTypefaceSpan(final Typeface typeface) {
+        this.mTypeface = typeface;
+    }
+
+    @Override
+    public void updateDrawState(final TextPaint drawState) {
+        apply(drawState);
+    }
+
+    @Override
+    public void updateMeasureState(final TextPaint paint) {
+        apply(paint);
+    }
+
+    private void apply(final Paint paint) {
+        final Typeface oldTypeface = paint.getTypeface();
+        final int oldStyle = oldTypeface != null ? oldTypeface.getStyle() : 0;
+        final int fakeStyle = oldStyle & ~mTypeface.getStyle();
+
+        if ((fakeStyle & Typeface.BOLD) != 0) {
+            paint.setFakeBoldText(true);
+        }
+
+        if ((fakeStyle & Typeface.ITALIC) != 0) {
+            paint.setTextSkewX(-0.25f);
+        }
+
+        paint.setTypeface(mTypeface);
+    }
+}
