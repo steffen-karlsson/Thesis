7bbe8643a226cf3e2a4c85c74110c45f1fc087be
Steffen Karlsson <steffen.karlsson@gmail.com>
2016-06-21 15:15:27 +0200

    removing apscheduler logging

--- code/sofa-project/sofa/foundation/scheduler.py
+++ code/sofa-project/sofa/foundation/scheduler.py
@@ -1,7 +1,15 @@
 # Created by Steffen Karlsson on 06-11-2016
 # Copyright (c) 2016 The Niels Bohr Institute at University of Copenhagen. All rights reserved.
 
+from logging import getLogger, ERROR
+
 from apscheduler.schedulers.background import BackgroundScheduler
+
+getLogger('apscheduler.executors.default').setLevel(ERROR)
+getLogger('apscheduler.scheduler.default').propagate = False
+
+getLogger('apscheduler.scheduler').setLevel(ERROR)
+getLogger('apscheduler.scheduler').propagate = False
 
 
 class BackgroundDaemonScheduler(BackgroundScheduler):
