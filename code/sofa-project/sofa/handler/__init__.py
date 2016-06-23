1397edf3cadac1b24a62a5d4ebc85b7091ece794
Steffen Karlsson <steffen.karlsson@gmail.com>
2016-06-23 11:03:46 +0200

    load function from source

--- code/sofa-project/sofa/handler/__init__.py
+++ code/sofa-project/sofa/handler/__init__.py
@@ -24,6 +24,17 @@ def get_class_from_path(path):
     return split_class_path(package)[1]
 
 
+def get_function_from_source(source, func_name):
+    def __load():
+        return globals()[func_name]
+
+    try:
+        return __load()
+    except AttributeError:
+        exec (source, globals())
+        return __load()
+
+
 def get_class_from_source(source, cls_name):
     def __instantiate():
         return eval("%s()" % cls_name)
