d719a6f8a361b231f65e7395ba9d492053edf8af
Steffen Karlsson <steffen.karlsson@gmail.com>
2016-07-14 13:44:29 +0200

    Uodating tests

--- code/bdae-project/examples/mobydick_word.py
+++ code/bdae-project/examples/mobydick_word.py
@@ -1,19 +1,36 @@
 # Created by Steffen Karlsson on 02-19-2016
 # Copyright (c) 2016 The Niels Bohr Institute at University of Copenhagen. All rights reserved.
 
-from sofa.foundation.base import load_data_by_url
+from sofa.foundation.base import load_data_by_path
 from bdae.templates.text_dataset import TextDataByWord
-from sofa.foundation.operation import OperationContext
+from sofa.foundation.operation import OperationContext, Sequential, Parallel
 from sofa.foundation.strategy import Tiles
+
 
 class MobyDickDatasetWord(TextDataByWord):
     def preprocess(self, data_ref):
-        return load_data_by_url(data_ref)
+        return load_data_by_path(data_ref)
+
+    def get_map_functions(self):
+        return super(MobyDickDatasetWord, self).get_map_functions() + [nothing]
 
     def get_operations(self):
         return [
-            OperationContext.by(self, "count", '[count_occurrences, sum]'),
+            OperationContext.by(self, "count (neighborhood test)",
+                                '[nothing, neighborhood:1:1, modify:block_formatter, count_occurrences, sum]'),
+            OperationContext.by(self, "count", '[modify:block_formatter, count_occurrences, sum]')
         ]
 
     def get_distribution_strategy(self):
         return Tiles(2)
+
+    def get_replication_factor(self):
+        return 1
+
+
+def block_formatter(block_gen):
+    return [" ".join(block) for block in block_gen]
+
+
+def nothing(blocks, args):
+    return blocks
--- code/bdae-project/examples/text.py
+++ code/bdae-project/examples/text.py
@@ -4,17 +4,22 @@
 from bdae.libpy.libbdaescientist import PyBDAEScientist
 
 from examples.mobydick_word import MobyDickDatasetWord
+from examples.mobydick_sentence import MobyDickDatasetSentence
 from bdae.libpy.libbdaemanager import PyBDAEManager
+from bdae.libweb import GatewayWebWrapper
 
 LINK = "https://dl.dropboxusercontent.com/u/10746829/melville-moby_dick.txt"
 
 if __name__ == '__main__':
     manager = PyBDAEManager("sofa:textdata:gateway:0")
     manager.create_dataset(MobyDickDatasetWord(name="moby dick word", description="Moby dick as words"))
+    # manager.create_dataset(MobyDickDatasetSentence(name="moby dick sentence", description="Moby dick as sentence"))
     manager.append_to_dataset("moby dick word", LINK)
 
     def callback(res):
         print "Count:", res
 
     scientist = PyBDAEScientist("sofa:textdata:gateway:0")
-    scientist.submit_job("moby dick word", "count", "Moby Dick", callback=callback)
+    # GatewayWebWrapper(scientist).start(9990, hostname='0.0.0.0')
+    scientist.submit_job("moby dick word", "count (neighborhood test)", "Moby Dick", callback=callback)
+    # scientist.submit_job("moby dick word", "count", "Moby Dick", callback=callback)
